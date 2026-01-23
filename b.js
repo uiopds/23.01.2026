f471c1ff4ec59315611fce54d56f6fdada351efb
uiopds <162052791+uiopds@users.noreply.github.com>
2026-01-23 13:28:26 +0300

    Add files via upload

--- b.js
+++ b.js
@@ -1,19 +1,25 @@
-let x; console.log('1. typeof x:', typeof x); // "undefined" обьявляем переменную, но не присваиваем значение
-
-console.log('2. typeof 10:', typeof 10, 'typeof "10":', typeof "10"); // "number" // "string"
-
-console.log('3. typeof true:', typeof true, 'typeof false:', typeof false); // "boolean" // "boolean"
-
-console.log('4. typeof null:', typeof null); // "object"
-
-console.log('5. "15" + 1 =', "15" + 1); // "151"
-
-console.log('6. "12" - 1 =', "12" - 1); // 11
-
-console.log('7. Boolean("") =', Boolean("")); // false
-
-console.log('8. Boolean(" ") =', Boolean(" ")); // true
-
-console.log('9. +" 13 " =', +" 13 "); console.log(' Number(" 13 ") =', Number(" 13 ")); console.log(' parseInt(" 13 ") =', parseInt(" 13 ")); // 13
-
-function isNumber(v) { return typeof v === 'number' && !isNaN(v)}; // Проверяем: 1) тип - number, 2) не NaN
+// 1
+let x; console.log(typeof x); // пустая переменная 
+// 2
+console.log(typeof 10);    // "number" - число 
+console.log(typeof "10");  // "string" - строка 
+// 3
+console.log(typeof true);   // "boolean" - булевые данные 
+console.log(typeof false);  // "boolean" - булевые данные 
+// 4 
+console.log(typeof null); // "object" - 
+// 5 
+console.log("15" + 1); // "151" - конкатенация строк
+// 6 
+console.log("12" - 1); // 11 -происходит арифметическая опирация вычитания 
+// 7 
+console.log(Boolean("")); // false - передаем пустую строку (ничего) 
+// 8 
+console.log(Boolean(" ")); // true мы передаем пробел, что считается символом 
+// 9 
+console.log(+" 13 "); // 13 -явное преобразование в число 
+// 10 
+function isNumber(v) {
+    return typeof v === 'number' && !isNaN(v);
+}
+console.log(isNumber(1))
